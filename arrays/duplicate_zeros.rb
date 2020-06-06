85c853fea079ef152943ba09816609157fb20dbb
SeanPMulligan <seanmulligan@protonmail.com>
2020-06-06 09:26:44 -0700

    Duplicate zeros problem

---
+++ arrays/duplicate_zeros.rb
@@ -0,0 +1,46 @@
+=begin
+https://leetcode.com/problems/duplicate-zeros/
+
+
+Given a fixed length array arr of integers, duplicate each occurrence of zero,
+shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place, do not return anything
+from your function.
+
+Example 1:
+
+Input: [1,0,2,3,0,4,5,0]
+Output: null
+
+Explanation: After calling your function, the input array is modified
+to: [1,0,0,2,3,0,0,4]
+
+Example 2:
+
+Input: [1,2,3]
+Output: null
+
+Explanation: After calling your function, the input array is modified
+to: [1,2,3]
+=end
+
+# @param {Integer[]} arr
+# @return {Void} Do not return anything, modify arr in-place instead.
+def duplicate_zeros(arr)
+  i = 0
+
+  while i < arr.length
+    if arr[i].zero?
+      arr.insert(i, 0)
+      i += 2
+      arr.pop
+    else
+      i += 1
+    end
+  end
+
+  arr
+end
